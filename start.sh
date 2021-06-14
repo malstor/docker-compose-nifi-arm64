e5101e87ea3df45e500e7790ca7927fb5e32a9ac
Jeremy Turner <jeremy@malstorm.com>
2021-06-14 12:40:36 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Great writeup here https://deparkes.co.uk/2019/11/16/nifi-version-control-with-nifi-registry/
 sudo apt-get update && sudo apt-get upgrade
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker ${USER}
@@ -8,4 +9,5 @@ sudo apt-get install -y python3 python3-pip
 sudo pip3 install docker-compose
 sudo systemctl enable docker
 git clone https://github.com/malstor/docker-compose-nifi-arm64.git
+# options to add more nodes: docker-compose up --scale nifi=3 -d
 docker-compose -f docker-compose.yaml up -d
